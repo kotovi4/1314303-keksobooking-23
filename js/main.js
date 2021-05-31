801f15e212d68499d6030def8ebd8752a44630d8
kotovi4 <kotovi4.m@gmail.com>
2021-05-31 15:51:38 +0300

    Вносит изменения в код функций

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-console.log(getRndInteger(0, 100));
+console.log(getRandomInt(0, 100));
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomFloat(min, max) {
+  return Math.random() * (max - min) + min;
 }
-console.log(Math.random());
+console.log(getRandomFloat(0, 100));
