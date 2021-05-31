71bc890ff4c535345a7c5a88805a4ab3be35de33
kotovi4 <kotovi4.m@gmail.com>
2021-05-31 15:20:30 +0300

    Добавляет фунции возврата рандомных чисел

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(getRndInteger(0, 100));
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(Math.random());
