77bfce683fec8b994fcab8bc2bd8a36de96a9005
kotovi4 <kotovi4.m@gmail.com>
2021-06-01 12:38:49 +0300

    Вносит изменения

--- js/main.js
+++ js/main.js
@@ -3,11 +3,22 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 getRandomInt();
-console.log(getRandomInt(0, 100));
+// console.log(getRandomInt(0, 100));
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 function getRandomFloat(min, max) {
   return Math.random() * (max - min) + min;
 }
 getRandomFloat();
-console.log(getRandomFloat(0, 100));
+console.log(getRandomFloat(0, 100).toFixed(2));
+
+
+let min = 60;
+let max = 40;
+
+if (min >= max) {
+  console.log('Все не очень');
+} else {
+  console.log('Все круто');
+}
+
