e44d5fa9713e8b86d373051affa2f29b241b3457
kotovi4 <kotovi4.m@gmail.com>
2021-06-01 19:31:09 +0300

    Вносит дополнения в функции со случаными числами

--- js/main.js
+++ js/main.js
@@ -1,24 +1,19 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  if (min >= 0 && max > min) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  return 'Error';
 }
 getRandomInt();
 // console.log(getRandomInt(0, 100));
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-function getRandomFloat(min, max) {
-  return Math.random() * (max - min) + min;
+function getRandomFloat(min, max, digits) {
+  if (min >= 0 && max > min && digits >= 0) {
+    return (Math.random() * (max - min) + min).toFixed(digits);
+  }
+  return 'Error';
 }
 getRandomFloat();
-console.log(getRandomFloat(0, 100).toFixed(2));
-
-
-let min = 60;
-let max = 40;
-
-if (min >= max) {
-  console.log('Все не очень');
-} else {
-  console.log('Все круто');
-}
-
+// console.log(getRandomFloat(0, 100, 5));
