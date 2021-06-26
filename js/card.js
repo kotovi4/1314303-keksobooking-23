33cb77863781a8a2e596838cfe16fb8fd51114bc
kotovi4 <kotovi4.m@gmail.com>
2021-06-26 17:46:38 +0300

    Добавляет вывод карточки

--- js/card.js
+++ js/card.js
@@ -1,16 +1,25 @@
-import {getOffers, NUMBER_OF_OFFERS} from './data.js';
+import {getOffers, NUMBER_OF_OFFERS, OFFER_TYPES} from './data.js';
 
 const cardListElement = document.querySelector('.map');
-const cardTemplate = document.querySelector('#card')
-  .content
-  .querySelector('.popup');
+const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 
 // const cardElement = cardTemplate.cloneNode(true);
 // cardListElement.appendChild(cardElement);
 
 const similarCards = getOffers(NUMBER_OF_OFFERS);
 
-similarCards.forEach(() => {
+const cardListFragment = document.createDocumentFragment();
+
+similarCards.forEach((offer) => {
   const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.popup__title').textContent = offer.title;
+  cardElement.querySelector('.popup__text--address').style.fill = offer.adress;
+  cardElement.querySelector('.popup__text--price').style.fill = `${offer.price} ₽/ночь`;
+  cardElement.querySelector('.popup__type').textContent = OFFER_TYPES(offer.type);
+  cardElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
   cardListElement.appendChild(cardElement);
 });
+
+cardListElement.appendChild(cardListFragment);
