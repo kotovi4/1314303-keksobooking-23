78b7b85f8ad4dcc4b0b0fa952bf0869fc42aaf85
kotovi4 <kotovi4.m@gmail.com>
2021-07-01 20:55:52 +0300

    Вносит изменения

--- js/card.js
+++ js/card.js
@@ -27,19 +27,19 @@ const fillTextContentProperties = (element, propertyOne, propertyTwo, textConten
   }
 };
 
-const fillSrc = (element, srcData) => {
+const fillSrc = (element, src) => {
   if (element.length === 0) {
     element.remove();
   } else {
-    element.src = srcData;
+    element.src = src;
   }
 };
 
-const fillPrice = (element, priceData) => {
-  if (priceData.length === 0) {
+const fillPrice = (element, price) => {
+  if (price.length === 0) {
     element.remove();
   } else {
-    element.textContent = `${priceData} ₽/ночь`;
+    element.textContent = `${price} ₽/ночь`;
   }
 };
 
@@ -72,7 +72,6 @@ const renderOffer = (card) => {
   const description = cardElement.querySelector('.popup__description');
   const type = cardElement.querySelector('.popup__type');
   const time = cardElement.querySelector('.popup__text--time');
-  const features = cardElement.querySelector('.popup__features');
   const avatar = cardElement.querySelector('.popup__avatar');
   const price = cardElement.querySelector('.popup__text--price');
   const capacity = cardElement.querySelector('.popup__text--capacity');
@@ -86,6 +85,7 @@ const renderOffer = (card) => {
   fillPrice(price, card.offer.price);
   fillTextContentProperties(capacity, card.offer.rooms, card.offer.guests, fillCapacity(offer));
 
+  const features = cardElement.querySelector('.popup__features');
   const modifiers = card.offer.features.map((feature) => `popup__feature--${feature}`);
   features.querySelectorAll('.popup__feature').forEach((item) => {
     const modifier = item.classList[1];
@@ -97,7 +97,6 @@ const renderOffer = (card) => {
 
   const popupPhoto = cardElement.querySelector('.popup__photos');
   const clonePhotoTemplate = popupPhoto.querySelector('img');
-
   for (let index = 0; index < card.offer.photos.length; index++) {
     const clonePhoto = clonePhotoTemplate.cloneNode(true);
     clonePhoto.src = card.offer.photos[index];
