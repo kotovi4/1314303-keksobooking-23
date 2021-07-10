24003603e12a7ea50fa585a41b9c951976fff968
kotovi4 <kotovi4.m@gmail.com>
2021-07-10 21:38:06 +0300

    ЗДобавляет код для валидации формы добавления объявления

--- js/form.js
+++ js/form.js
@@ -1,38 +1,99 @@
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const MAX_PRICE_PER_NIGHT = 1000000;
+
+const minPrices = {
+  'flat': 1000,
+  'palace': 10000,
+  'house': 5000,
+  'bungalow': 0,
+};
+
 const adForm = document.querySelector('.ad-form');
-const mapFilters = document.querySelector('.map__filters');
+const titleInput = adForm.querySelector('#title');
+const priceInput = adForm.querySelector('#price');
+const typeField = adForm.querySelector('#type');
 
-const disabledMapForm = () => {
-  adForm.classList.add('ad-form--disabled');
-  adForm.querySelectorAll('fieldset').forEach((fieldset) => {
-    fieldset.disabled = true;
+// const disabledMapForm = () => {
+//   adForm.classList.add('ad-form--disabled');
+//   adForm.querySelectorAll('fieldset').forEach((fieldset) => {
+//     fieldset.disabled = true;
+//   });
+
+//   mapFilters.classList.add('map__filters--disabled');
+//   mapFilters.querySelectorAll('.map__filter').forEach((filter) => {
+//     filter.disabled = true;
+//   });
+//   mapFilters.querySelectorAll('.map__features').forEach((feature) => {
+//     feature.disabled = true;
+//   });
+// };
+
+// const activateMapForm = () => {
+//   adForm.classList.remove('ad-form--disabled');
+
+//   adForm.querySelectorAll('fieldset').forEach((fieldset) => {
+//     fieldset.disabled = false;
+//   });
+
+//   mapFilters.classList.remove('map__filters--disabled');
+//   mapFilters.querySelectorAll('.map__filter').forEach((filter) => {
+//     filter.disabled = false;
+//   });
+//   mapFilters.querySelectorAll('.map__features').forEach((feature) => {
+//     feature.disabled = false;
+//   });
+// };
+
+if (titleInput) {
+  titleInput.addEventListener('invalid', () => {
+    if (titleInput.validity.valueMissing) {
+      titleInput.setCustomValidity('Обязательное поле');
+    } else {
+      titleInput.setCustomValidity('');
+    }
   });
 
-  mapFilters.classList.add('map__filters--disabled');
-  mapFilters.querySelectorAll('.map__filter').forEach((filter) => {
-    filter.disabled = true;
-  });
-  mapFilters.querySelectorAll('.map__features').forEach((feature) => {
-    feature.disabled = true;
-  });
-};
+  titleInput.addEventListener('input', () => {
+    const valueLength = titleInput.value.length;
 
-const activateMapForm = () => {
-  adForm.classList.remove('ad-form--disabled');
+    if (valueLength < MIN_TITLE_LENGTH) {
+      titleInput.setCustomValidity(`Минимальная длина ${MIN_TITLE_LENGTH} символов. Нужно ввести ещё ${MIN_TITLE_LENGTH - valueLength}`);
+    } else if (valueLength > MAX_TITLE_LENGTH) {
+      titleInput.setCustomValidity(`Максимальная длина не может быть больше ${MAX_TITLE_LENGTH} символов. Удалите лишние ${valueLength - MAX_TITLE_LENGTH} симв.`);
+    } else {
+      titleInput.setCustomValidity('');
+    }
+  });
+  titleInput.reportValidity();
+}
 
-  adForm.querySelectorAll('fieldset').forEach((fieldset) => {
-    fieldset.disabled = false;
+if (priceInput) {
+  priceInput.addEventListener('invalid', () => {
+    if (priceInput.validity.valueMissing) {
+      priceInput.setCustomValidity('Обязательное поле');
+    } else {
+      priceInput.setCustomValidity('');
+    }
   });
 
-  mapFilters.classList.remove('map__filters--disabled');
-  mapFilters.querySelectorAll('.map__filter').forEach((filter) => {
-    filter.disabled = false;
-  });
-  mapFilters.querySelectorAll('.map__features').forEach((feature) => {
-    feature.disabled = false;
-  });
-};
+  priceInput.addEventListener('input', () => {
+    const price =priceInput.value;
+    const type = typeField.value;
+    const minPrice = minPrices[type];
 
-export {
-  disabledMapForm,
-  activateMapForm
-};
+    if (price < minPrice) {
+      priceInput.setCustomValidity(`Стоимость должна быть не менее ${minPrice}`);
+    } else if (price > MAX_PRICE_PER_NIGHT) {
+      priceInput.setCustomValidity(`Стоимость не должна превышать ${MAX_PRICE_PER_NIGHT}`);
+    } else {
+      priceInput.setCustomValidity('');
+    }
+  });
+  priceInput.reportValidity();
+}
+
+// export {
+//   disabledMapForm,
+//   activateMapForm
+// };
